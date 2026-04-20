4fbe330f2689c5eccede08cd2698f2ccb89d3abd
Konirol <shuaevgasan@gmail.com>
2026-04-20 18:31:45 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const products = [
+    {
+        id: 1,
+        productName: 'Laptop',
+        price: 1000,
+        description: 'DESC'
+    },
+    {
+        id: 2,
+        productName: 'Laptop',
+        price: 1000,
+        description: 'DESC'
+    },
+    {
+        id: 3,
+        productName: 'Laptop',
+        price: 1000,
+        description: 'DESC'
+    },
+    {
+        id: 4,
+        productName: 'Laptop',
+        price: 1000,
+        description: 'DESC'
+    },
+]
